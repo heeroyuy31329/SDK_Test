685e23e76abf91ed2cfd98599ce1035870c47dce
姜 <heeroyuy31329@gmail.com>
2021-11-03 16:08:17 +0800

    增加SMP對應

---
+++ Sources/SDKTest2.xcframework/ios-arm64_i386_x86_64-simulator/SDKTest2.framework/Headers/SDKTest2.h
@@ -0,0 +1,18 @@
+//
+//  SDKTest2.h
+//  SDKTest2
+//
+//  Created by 姜宗暐 on 2021/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKTest2.
+FOUNDATION_EXPORT double SDKTest2VersionNumber;
+
+//! Project version string for SDKTest2.
+FOUNDATION_EXPORT const unsigned char SDKTest2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKTest2/PublicHeader.h>
+
+
